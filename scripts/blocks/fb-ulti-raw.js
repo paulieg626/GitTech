56629012b89a592de16fd7cffa90fff7ae5ab393
paulieg626 <tvxakiminecraft@gmail.com>
2020-06-02 18:46:55 +0500

    update

---
+++ scripts/blocks/fb-ulti-raw.js
@@ -0,0 +1,27 @@
+const multiLib=require("wrapper");
+const fb_ulti_raw = multiLib.extend(GenericCrafter,GenericCrafter.GenericCrafterEntity,"fb-ulti-raw",{},{
+  _output:[
+[[["gittech-ulti_raw",1]],null,null]
+],
+  _input:[
+[null,[["water",30]],null]
+],
+  craftTimes:[120],
+  output:[],
+  input:[],
+  itemList:[],
+  liquidList:[],
+  isSameOutput:[],
+});
+
+fb_ulti_raw.enableInv=false;
+fb_ulti_raw.dumpToggle=false;
+fb_ulti_raw.localizedName="Утилизатор";
+fb_ulti_raw.description="Утилизирует воду";
+fb_ulti_raw.itemCapacity= 5;
+fb_ulti_raw.liquidCapacity= 40;
+fb_ulti_raw.size= 2;
+fb_ulti_raw.health= 100;
+fb_ulti_raw.craftEffect= Fx.pulverizeMedium;
+fb_ulti_raw.updateEffect=Fx.none;
+fb_ulti_raw.requirements(Category.crafting,ItemStack.with(Items.copper,50));
--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,5 @@ require("blocks/fb-bronze");
 require("blocks/fb-tin-melting");
 require("blocks/fb-tin-shaper");
 require("blocks/fb-sifting");
+require("blocks/fb-ulti-raw");
+require("blocks/fb-boxer");
