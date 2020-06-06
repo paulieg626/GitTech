5b018e7ddba6f4b07873d310958ec6d77a109c11
paulieg626 <tvxakiminecraft@gmail.com>
2020-06-06 19:28:18 +0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
 require("blocks/fb-drob");
-//require("blocks/fb-bronze");
-//require("blocks/fb-tin-melting");
 require("blocks/fb-tin-shaper");
 require("blocks/fb-sifting");
 require("blocks/fb-ulti-raw");
